401c99085d7c294dee365d3c6d74338af2f13a54
Yanis Bouchenna <bouchenna.yanis@protonmail.com>
2020-10-03 13:57:14 +0100

    :construction: Fix relative paths

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://karm0s.github.io/FlashCode'
-    :  '/FlashCode'
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? 'https://karm0s.github.io/FlashCode'
+//     :  '/FlashCode'
 
+// }
+module.exports = {
+  publicPath: 'https://karm0s.github.io/FlashCode'
 }
