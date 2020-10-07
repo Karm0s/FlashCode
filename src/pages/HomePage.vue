30e56a2b0b8f9350e0bc6c92fe350617497b941a
Yanis Bouchenna <bouchenna.yanis@protonmail.com>
2020-10-07 14:22:44 +0100

    :lipstick: Update components list

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,12 +5,9 @@
     <div class="flex flex-col items-start mx-10 mt-20">
       <h1 class="text-xl text-gray-900 title tracking-tight">Components:</h1>
 
-      <li class="ml-10 mt-5">
-        <ul>
-          <router-link to="/dragndrop" class="text-lg hover:underline">Drag n Drop <span class="fas fa-arrow-right"></span> </router-link>
-          <!-- <a href="/dragndrop" class="text-lg hover:underline">Drag n Drop <span class="fas fa-arrow-right"></span> </a> -->
-        </ul>
-      </li>
+      <div class="ml-10 mt-5">
+          <router-link to="/dragndrop" class="text-lg hover:underline">Drag n Drop <span class="fas fa-arrow-right"></span></router-link>
+      </div>
     </div>
   </div>
 </template>
